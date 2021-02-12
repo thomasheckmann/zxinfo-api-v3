53da81f8b8dbff108aa1abf7e526c162cba671f3
Thomas Heckmann <thomas.kolbeck@gmail.com>
2021-02-12 12:02:04 +0100

    mc

--- routes/social.js
+++ routes/social.js
@@ -125,7 +125,7 @@ router.get("/details/:gameid", (req, res) => {
       }
       // og_image = "https://ebimg.dk/ux/data/social/eblogo_1024.png";
       var html = `<html><head><title>${og_title} | ZXInfo</title>`;
-      html += `<meta property="og:url" content="https://zxinfo.dk/details/0002259" />`;
+      html += `<meta property="og:url" content="https://zxinfo.dk/details/${id}" />`;
       html += `<meta property="og:type" content="article" />`;
       html += `<meta property="og:title" content="${og_title}" />`;
       html += `<meta property="og:description" content="${og_description}" />`;
