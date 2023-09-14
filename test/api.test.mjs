5e0fa07093889761df2a32a7266587677f0d3a15
thomasheckmann <thomas.kolbeck@gmail.com>
2023-09-14 21:18:36 +0200

    update test case

--- test/api.test.mjs
+++ test/api.test.mjs
@@ -14,15 +14,6 @@ describe("Testing API endpoint", function () {
       assert.equal(body.machinetypes.values.length >= 30, true);
     })
 
-    await t.test("testing /metadata", async (t) => {
-      const request = await fetch(API_ENDPOINT + "/metadata", {
-        method: 'GET',
-      });
-      assert.strictEqual(request.status, 200);
-      const body = await request.json();
-      assert.equal(body.machinetypes.values.length >= 30, true);
-    })
-
     await t.test("testing /games - 2259", async (t) => {
       const request = await fetch(API_ENDPOINT + "/games/2259?mode=tiny", {
         method: 'GET',
@@ -32,7 +23,7 @@ describe("Testing API endpoint", function () {
       assert.deepStrictEqual(body._id, "0002259");
     })
 
-    await t.test("testing /games - 0002259", async (t) => {
+    await t.test("testing /games - 0002259 (prefixed with zero's)", async (t) => {
       const request = await fetch(API_ENDPOINT + "/games/0002259?mode=tiny", {
         method: 'GET',
       });
@@ -60,6 +51,31 @@ describe("Testing API endpoint", function () {
       const body = await request.json();
       assert.deepStrictEqual(body.entry_id, "0028617");
       assert.deepStrictEqual(body.file[0].source, "ZX81 STUFF");
+    })
+
+  });
+
+  test("running tests on SOURCES", async function (t) {
+    const API_ENDPOINT = "http://localhost:8300/v3";
+
+    await t.test("SOURCE: ZX81 STUFF, testing 3D Monster Maze(0028617)", async (t) => {
+      const request = await fetch(API_ENDPOINT + "/filecheck/412b28086cbe44d3054b8649c43c67c4318f46e7a9e8f35e7468e734f934d541390aced5a1f486beb4929cb2440a557fcbd10a41703e292834b386e4490fa512", {
+        method: 'GET',
+      });
+      assert.strictEqual(request.status, 200);
+      const body = await request.json();
+      assert.deepStrictEqual(body.entry_id, "0028617");
+      assert.deepStrictEqual(body.file[0].source, "ZX81 STUFF");
+    })
+
+    await t.test("SOURCE: NVG, 180(0000015)", async (t) => {
+      const request = await fetch(API_ENDPOINT + "/filecheck/eaf4781b93906240eb929aac582c5126c2a915280e3f8c4162217c616ef93526eca82fda66977be66baf07f5389fd1a8d5758cc62995416169f05bcc6d30933a", {
+        method: 'GET',
+      });
+      assert.strictEqual(request.status, 200);
+      const body = await request.json();
+      assert.deepStrictEqual(body.entry_id, "0000015");
+      assert.deepStrictEqual(body.file[0].source, "ftp.nvg.unit.no");
     })
 
   });
